5cf1a85fae252c4eae4c6e90b6941d0e9b3d0df7
Patrick McDonagh <patrick@meshify.com>
2021-08-23 15:31:05 -0500

    use env variable for reset pin

--- start_common.sh
+++ start_common.sh
@@ -8,7 +8,6 @@ elif [ $TTN_STACK_VERSION -eq 3 ]; then
 	TC_URI=${TC_URI:-"wss://${TTN_REGION}.cloud.thethings.network:8887"} 
 else
     echo -e "\033[91mERROR: Wrong TTN_STACK_VERSION value, should be either 2 o 3.\033[0m"
-	balena-idle
 fi
 
 # Get certificate
@@ -21,7 +20,6 @@ TC_TRUST=$(echo $TC_TRUST | sed 's/\s//g' | sed 's/-----BEGINCERTIFICATE-----/--
 if [ "$TC_URI" == "" ] || [ "$TC_TRUST" == "" ]
 then
     echo -e "\033[91mERROR: Missing configuration, define either TTN_STACK_VERSION or TC_URI and TC_TRUST.\033[0m"
-	balena-idle
 fi
 
 echo "Server: $TC_URI"
